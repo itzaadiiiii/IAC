0853081382f9e423caa3c4b33e17fea05660781b
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-05-23 23:22:25 +0530

    DG-37: (Add) Azure TF provider.tf provider version updated

--- Terraform/Azure/provider.tf
+++ Terraform/Azure/provider.tf
@@ -3,7 +3,8 @@
     required_providers {
         azurerm = {
         source  = "hashicorp/azurerm"
-        version = "=3.0.0"
+        # version = "=3.0.0"
+        version = "4.74.0"
         }
     }
     }
