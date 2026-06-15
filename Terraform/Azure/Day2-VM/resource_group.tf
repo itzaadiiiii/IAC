c810c1a788879174e251dbc917127463d328e127
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-06-16 00:00:17 +0530

    Azure Terraform Day-2 resource_group.tf

---
+++ Terraform/Azure/Day2-VM/resource_group.tf
@@ -0,0 +1,6 @@
+    ## resources-group.tf
+    # Create a resource group
+    resource "azurerm_resource_group" "dev_eu_north_rg" {
+    name     = "dev-eu-north-rg"
+    location = "North Europe"
+    } 
