c3f81c16fd2488bcf85fc88220329780f4b5b992
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-07-13 11:51:59 +0530

    Update providers.tf

--- Terraform/AWS/Terraform - PYSH/Day-16 IAM User Management/providers.tf
+++ Terraform/AWS/Terraform - PYSH/Day-16 IAM User Management/providers.tf
@@ -1,3 +1,20 @@
-provider "aws" {
-  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.54.0"
+    }
+  }
 }
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"   #Mumbai
+  alias = "Primary"
+}
+
+# provider "aws" {
+#   # Configuration options
+#   region = "ap-south-2"    #Hydrabad
+#   alias = "Secondary"
+# }
