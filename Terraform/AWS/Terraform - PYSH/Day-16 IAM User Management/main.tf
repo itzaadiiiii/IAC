45dd73d0ecf41fb4fa222c0acb9dc4a4f8a1dd3c
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-07-13 23:33:53 +0530

    Update main.tf

--- Terraform/AWS/Terraform - PYSH/Day-16 IAM User Management/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-16 IAM User Management/main.tf
@@ -1,5 +1,4 @@
 resource "aws_iam_user" "iam_user" {
-  for_each = [ for user in local.users : user ]
-
-  name = "each.value"
+for_each = [ for user in local.users : "${user.name} ${user.last_name}"]
+    name = "each.value"
 }
