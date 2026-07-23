09f52537a4d59b95f10a314f0cf63b9c0f83e7bc
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-07-23 23:57:57 +0530

    Add VPC block for the primary region

--- Terraform/AWS/Terraform - PYSH/Day-15 VPC Peering/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-15 VPC Peering/main.tf
@@ -1,17 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.55.0"
-    }
-  }
-}
-provider "aws" {
-  region = "us-east-1"
-  alias = var.primary
-}
+resource "aws_vpc" "main" {
+    cidr_block       = "10.0.0.0/16"
+    instance_tenancy = "default"
+    provider = aws.primary
 
-provider "aws" {
-    region = "us-west-2"
-    alias = var.secondary
+    tags = {
+        Name = "main"
+    }
 }
