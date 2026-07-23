b8cf28f21fa6f3e8b126e5ab3476be2d5546d9df
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-07-23 22:42:29 +0530

    Add backend.tf

--- Terraform/AWS/Terraform - PYSH/Day-15 VPC Peering/backend.tf
+++ Terraform/AWS/Terraform - PYSH/Day-15 VPC Peering/backend.tf
@@ -1,9 +1,18 @@
-terraform {
-  backend "remote" {
-    organization = "aadiizworld"
+# terraform {
+#   backend "remote" {
+#     organization = "aadiizworld"
 
-    workspaces {
-      name = "Jan-2026"
+#     workspaces {
+#       name = "Jan-2026"
+#     }
+#   }
+# }
+
+terraform {
+    backend "s3" {
+        bucket       = "terraform-bucketttttt"
+        key          = "Terraform-State"
+        region       = "us-east-1"
+        use_lockfile = true
     }
-  }
 }
