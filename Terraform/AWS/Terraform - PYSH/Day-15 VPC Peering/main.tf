438624760d531de871d9d77f59259c5ff34e3270
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-07-23 23:59:45 +0530

    Variablized VPC_CIDR

--- Terraform/AWS/Terraform - PYSH/Day-15 VPC Peering/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-15 VPC Peering/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-    cidr_block       = "10.0.0.0/16"
+    cidr_block       = var.cidr_block
     instance_tenancy = "default"
     provider = aws.primary
     enable_dns_support = true
