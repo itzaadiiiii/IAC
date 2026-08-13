f2600e42cc0015414f3b62aa6371da4a87d70e04
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-13 22:36:58 +0530

    Fixed indentation

--- Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/backend.tf
+++ Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/backend.tf
@@ -1,10 +1,10 @@
 # Added S3 for Backend
 terraform {
-  backend "s3" {
-    # bucket       = var.tf_backend_bucket
-    bucket = "terraform-buckettttttttt"
-    key          = "Terraform-State"
-    region       = "ap-south-1"   #Mumbai
-    use_lockfile = true
-  }
+    backend "s3" {
+        # bucket       = var.tf_backend_bucket
+        bucket = "terraform-buckettttttttt"
+        key          = "Terraform-State"
+        region       = "ap-south-1"   #Mumbai
+        use_lockfile = true
+    }
 }
