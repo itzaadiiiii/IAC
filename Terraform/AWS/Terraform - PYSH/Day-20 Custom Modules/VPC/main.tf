f5f6968502722f9e44eb0e14fd82e5da0ba33c25
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-14 00:23:46 +0530

    Moved the data block to root/main.tf

--- Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/VPC/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/VPC/main.tf
@@ -11,10 +11,6 @@ resource "aws_vpc" "main" {
         })
     }
 }
-# If we apply the VPC you can fetch the AVAILABILITY ZONES using the below data source. This will help us to create subnets in different availability zones.
-data "aws_availability_zones" "available" { 
-    state = "available"
-}   
 
 #Add Internet Gateway
 resource "aws_internet_gateway" "main" {
