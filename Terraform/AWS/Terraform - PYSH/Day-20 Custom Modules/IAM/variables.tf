9d62a91d9061f11e4ae5a4c20699dd3bef927723
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-15 23:49:22 +0530

    Fixed Indentation

--- Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/IAM/variables.tf
+++ Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/IAM/variables.tf
@@ -40,16 +40,16 @@ resource "aws_iam_role" "node_group" {
 
 # Attach required policies to node group role
 resource "aws_iam_role_policy_attachment" "node_worker_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node_group.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role       = aws_iam_role.node_group.name
 }
 
 resource "aws_iam_role_policy_attachment" "node_cni_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node_group.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    role       = aws_iam_role.node_group.name
 }
 
 resource "aws_iam_role_policy_attachment" "node_registry_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node_group.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role       = aws_iam_role.node_group.name
 }
