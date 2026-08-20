4d0fa5675207099a4d739a4fd06b5560a7a1fb9f
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-20 23:42:12 +0530

    Add resources inside EKS/main.tf KMS alias and CloudWatch log group

--- Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/EKS/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/EKS/main.tf
@@ -13,3 +13,16 @@ resource "aws_kms_key" "eks" {
         }
     )
 }
+
+resource "aws_kms_alias" "eks" {
+    name          = "alias/${var.cluster_name}-eks"
+    target_key_id = aws_kms_key.eks.key_id
+}
+
+# CloudWatch Log Group for EKS
+resource "aws_cloudwatch_log_group" "eks" {
+    name              = "/aws/eks/${var.cluster_name}/cluster"
+    retention_in_days = 7
+
+    tags = var.tags
+}
