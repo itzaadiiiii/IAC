9d3d8d2431ee9d7283f3b0237e82716c968f9f6b
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-20 23:48:39 +0530

    Add Resource open_id_connect provider

--- Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/EKS/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-20 Custom Modules/EKS/main.tf
@@ -142,3 +142,23 @@ resource "aws_eks_cluster" "main" {
 
     tags = var.tags
 }
+
+# OIDC Provider for IRSA (IAM Roles for Service Accounts)
+data "tls_certificate" "cluster" {
+    count = var.enable_irsa ? 1 : 0
+    url   = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "cluster" {
+    count           = var.enable_irsa ? 1 : 0
+    client_id_list  = ["sts.amazonaws.com"]
+    thumbprint_list = [data.tls_certificate.cluster[0].certificates[0].sha1_fingerprint]
+    url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+
+    tags = merge(
+        var.tags,
+        {
+        Name = "${var.cluster_name}-oidc-provider"
+        }
+    )
+}
