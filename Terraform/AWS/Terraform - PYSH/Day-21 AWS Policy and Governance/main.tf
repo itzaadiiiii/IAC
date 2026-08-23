2e28eeed6018272497a84bfba59bd5a1f6128624
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-23 23:40:16 +0530

    Add bucket resource to store the AWS config logs and compliance details

---
+++ Terraform/AWS/Terraform - PYSH/Day-21 AWS Policy and Governance/main.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "${var.project_name}-terraform-state"
+    acl    = "private"
+
+    versioning {
+        enabled = true
+    }
+
+    server_side_encryption_configuration {
+        rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+        }
+    }
+
+    tags = {
+        Name        = "Terraform State Bucket"
+        Environment = "Governance"
+        Managed_by  = "Terraform"
+        Purpose     = "AWS-Config_Storeage"
+    }
+}
