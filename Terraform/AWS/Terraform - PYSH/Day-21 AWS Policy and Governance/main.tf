002bcb8f2d0f4ba7edd68ef7cc1ebb3df39b5ed5
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-24 00:28:01 +0530

    Add resource block public acccess

--- Terraform/AWS/Terraform - PYSH/Day-21 AWS Policy and Governance/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-21 AWS Policy and Governance/main.tf
@@ -4,18 +4,10 @@ resource "random_string" "bucket_suffix" {
     upper   = false
 } 
 
-
+# Create an S3 bucket for AWS Config to store configuration history and snapshots
 resource "aws_s3_bucket" "config_bucket" {
     bucket = "${var.project_name}-config-bucket"
-    acl    = "private"
-
-    server_side_encryption_configuration {
-        rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
-        }
-    }
+    acl    = "private
 
     tags = {
         Name        = "Terraform Config Bucket"
@@ -51,3 +43,55 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "config_bucket_enc
 #     target_bucket = aws_s3_bucket.config_bucket.id
 #     target_prefix = "log/"
 # }
+# block bucket public access for the S3 bucket
+resource "aws_s3_bucket_public_access_block" "config_bucket_public_access_block" {
+    bucket = aws_s3_bucket.config_bucket.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
+
+# Add bucket policy to block public access and allow only specific IAM roles to access the bucket
+resource "aws_s3_bucket_policy" "config_bucket_policy" {
+    bucket = aws_s3_bucket.config_bucket.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Deny"
+                Principal = "*"
+                Action = "s3:*"
+                Resource = [
+                    "${aws_s3_bucket.config_bucket.arn}/*",
+                    aws_s3_bucket.config_bucket.arn
+                ]
+                Condition = {
+                    Bool = {
+                        "aws:SecureTransport" = "false"
+                    }
+                }
+            },
+            {
+                Effect = "Allow"
+                Principal = {
+                    AWS = [
+                        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.config_role_name}"
+                    ]
+                }
+                Action = [
+                    "s3:GetObject",
+                    "s3:PutObject",
+                    "s3:DeleteObject",
+                    "s3:ListBucket"
+                ]
+                Resource = [
+                    "${aws_s3_bucket.config_bucket.arn}/*",
+                    aws_s3_bucket.config_bucket.arn
+                ]
+            }
+        ]
+    })
+}
