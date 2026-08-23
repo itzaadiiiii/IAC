c583da401935954ca331a8f6394d673a05abc4cd
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-24 00:18:22 +0530

    Add resouce for bucket encryption

--- Terraform/AWS/Terraform - PYSH/Day-21 AWS Policy and Governance/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-21 AWS Policy and Governance/main.tf
@@ -33,3 +33,21 @@ resource "aws_s3_bucket_versioning" "config_bucket_versioning" {
         status = "Enabled"
     }
 }
+# Here you can use your costamized KMS key for encryption, but for simplicity, we are using the default S3 encryption.
+
+# Enable encryption for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "config_bucket_encryption" {
+    bucket = aws_s3_bucket.config_bucket.id
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+}
+
+# # Enable logging for the S3 bucket but you neet to create a another bucket for logging, so we are not enabling it here.
+# resource "aws_s3_bucket_logging" "config_bucket_logging" {
+#     bucket = aws_s3_bucket.config_bucket.id
+#     target_bucket = aws_s3_bucket.config_bucket.id
+#     target_prefix = "log/"
+# }
