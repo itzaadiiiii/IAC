80c92dec10f49e51e5bb79a4889251449460cf39
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-08-30 23:55:13 +0530

    Fixed indentation

--- Terraform/AWS/IAM VPC EKS Using Modules/Modules/IAM/main.tf
+++ Terraform/AWS/IAM VPC EKS Using Modules/Modules/IAM/main.tf
@@ -1,18 +1,18 @@
 resource "aws_iam_role" "master" {
-  name = "ed-eks-master"
+    name = "ed-eks-master"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
+    assume_role_policy = <<POLICY
     {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+        }
+    ]
 }
 POLICY
 }
