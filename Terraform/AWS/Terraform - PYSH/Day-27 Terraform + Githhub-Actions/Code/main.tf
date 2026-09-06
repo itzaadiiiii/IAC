e5a88107f76668e7ca1213402439311cca98aa5e
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-09-06 23:12:37 +0530

    Modified terraform version inside Day-27

--- Terraform/AWS/Terraform - PYSH/Day-27 Terraform + Githhub-Actions/Code/main.tf
+++ Terraform/AWS/Terraform - PYSH/Day-27 Terraform + Githhub-Actions/Code/main.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,6 +9,8 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  required_version = ">= 1.14.0"
 }
 
 provider "aws" {
