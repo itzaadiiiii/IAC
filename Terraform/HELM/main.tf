2060ed5f2e3e3c7be4b432955b5bbd4a7e5c70e1
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-09-21 19:55:40 +0530

    Add helm chart resource for nginx controller from bitnami

--- Terraform/HELM/main.tf
+++ Terraform/HELM/main.tf
@@ -27,3 +27,16 @@ provider "helm" {
 ]
 }
 
+# 
+resource "helm_release" "nginx_ingress" {
+name       = "nginx-ingress-controller"
+repository = "https://charts.bitnami.com/bitnami"
+chart      = "nginx-ingress-controller"
+
+set = [
+{
+    name  = "service.type"
+    value = "ClusterIP"
+}
+]
+}
