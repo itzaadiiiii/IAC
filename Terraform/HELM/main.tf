b7b6ecbbbf3ddba82985d7040671939379704021
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-09-21 19:54:29 +0530

    Add helm repository details

--- Terraform/HELM/main.tf
+++ Terraform/HELM/main.tf
@@ -13,3 +13,17 @@ provider "helm" {
     config_path = "~/.kube/config"
     }
 }
+    registries = [
+{
+    url      = "oci://localhost:5000"
+    username = "username"
+    password = "password"
+},
+{
+    url      = "oci://private.registry"
+    username = "username"
+    password = "password"
+}
+]
+}
+
