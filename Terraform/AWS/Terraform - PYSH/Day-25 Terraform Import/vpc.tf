3b887623fe8f53f0b8e7b9d906381e2a8424d8fc
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-09-23 23:47:08 +0530

    Add resource SG

--- Terraform/AWS/Terraform - PYSH/Day-25 Terraform Import/vpc.tf
+++ Terraform/AWS/Terraform - PYSH/Day-25 Terraform Import/vpc.tf
@@ -3,3 +3,45 @@ variable "vpc_id" {}
 data "aws_vpc" "selected" {
     id = var.vpc_id
 }
+
+# Lets allow ingress for HTTP, HTTPS And SSH traffic from anywhere and all outbound traffic
+
+resource "aws_security_group" "allow_tls" {
+    name        = "allow_tls"
+    description = "Allow TLS inbound traffic and all outbound traffic"
+    vpc_id      = data.aws_vpc.selected.id
+
+    tags = {
+        Name = "allow_tls"
+    }
+
+    ingress {
+        from_port        = 443
+        to_port          = 443
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        # ipv6_cidr_blocks = ["::/0"]
+    }
+}
+
+
+
